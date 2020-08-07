2d8bbae534d4181697746233c6433f649b44da24
Welton Felix <contato.weltonf@gmail.com>
2020-08-06 23:42:09 -0300

    Aula 03

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,33 +3,55 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../assets/services/api';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  name: string;
+  bio: string;
+  avatar: string;
+  subject: string;
+  cost: number;
+  whatsapp: string;
+};
+
+interface TeacherItemProps {
+  teacher: Teacher;
+};
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+  }
+
   return(
     <article className='teacher-item'>
       <header>
-        <img src='https://avatars0.githubusercontent.com/u/52381662?s=460&u=67b6c3805001173bf3324ecbbbcf11f9f0dcb152&v=4' alt='Welton Felix'/>
+        <img src={teacher.avatar} alt={teacher.name}/>
         <div>
-          <strong>Welton Felix</strong>
-          <span>Física</span>
+          <strong>{ teacher.name }</strong>
+          <span>{ teacher.subject }</span>
         </div>
       </header>
 
-      <p>
-        Electronics student at IFPE. Passionate about Technology and Entrepreneurship.
-        <br/><br/>
-        Hi, I'm Welton, a Electronics student passionate about development and entrepreneurship.
-      </p>
+      <p>{ teacher.bio }</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ { teacher.cost }</strong>
         </p>
-        <button type='button'>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={createNewConnection}
+          href={`https://wa.me/${ teacher.whatsapp }`}
+        >
           <img src={whatsappIcon} alt='Whatsapp'/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
