7da3784a6a30813f98c0e759c43b685653c19e72
Welton Felix <contato.weltonf@gmail.com>
2020-08-08 16:17:18 -0300

    Create server deploy settings

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 
@@ -9,4 +10,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
