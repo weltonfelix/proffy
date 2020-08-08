00ff22be8453fe65a624e80b6c93bb073c1aa877
Welton Felix <contato.weltonf@gmail.com>
2020-08-08 15:33:06 -0300

    Create web v1.0

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+import api from '../../assets/services/api';
+
+export interface Teacher {
+  id: number;
+  name: string;
+  bio: string;
+  avatar: string;
+  subject: string;
+  cost: number;
+  whatsapp: string;
+};
+
+interface TeacherItemProps {
+  teacher: Teacher;
+};
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+  }
+
+  return(
+    <article className='teacher-item'>
+      <header>
+        <img src={teacher.avatar} alt={teacher.name}/>
+        <div>
+          <strong>{ teacher.name }</strong>
+          <span>{ teacher.subject }</span>
+        </div>
+      </header>
+
+      <p>{ teacher.bio }</p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ { teacher.cost }</strong>
+        </p>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={createNewConnection}
+          href={`https://wa.me/${ teacher.whatsapp }`}
+        >
+          <img src={whatsappIcon} alt='Whatsapp'/>
+          Entrar em contato
+        </a>
+      </footer>
+    </article>
+  );
+}
+
+export default TeacherItem;
