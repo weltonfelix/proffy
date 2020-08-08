ab597dca354fdeb290cddafb72d536599d915d4b
Welton Felix <contato.weltonf@gmail.com>
2020-08-08 17:19:45 -0300

    Update API URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.API_URL,
+  baseURL: 'https://proffy-v1.herokuapp.com/',
 });
 
 export default api;
